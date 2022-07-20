a5dce3956fda11eb6fbb0c35a2f5eaed85562334
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-07-20 17:39:37 +0200

    Handle fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import Blogs from "./BlogsList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDeleteBlogs = (id) => {
     const newBlogs = blogs.filter((element) => element.id !== id);
@@ -11,18 +12,30 @@ const Home = () => {
 
   useEffect(() => {
     const fetchResponse = (response) => {
+      if (!response.ok) {
+        throw Error("Couldn't fetch data please try again");
+      }
       return response.json();
     };
     const setResponse = (data) => {
       setBlogs(data);
       setIsPending(false);
+      setError(null);
     };
 
-    fetch("http://localhost:8000/blogs").then(fetchResponse).then(setResponse);
+    const handleFetchError = (error) => {
+      setError(error.message);
+      setIsPending(false);
+    };
+    fetch("http://localhost:8000/blogs")
+      .then(fetchResponse)
+      .then(setResponse)
+      .catch(handleFetchError);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {
         /* All blogs */
